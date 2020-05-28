b404893cc7458337a2b8be6dad8b670a20b87b6e
YEONHWAJO <63631237+YEONHWAJO@users.noreply.github.com>
2020-05-28 20:34:14 +0900

    Update Groups.java

--- src/main/java/com/kh/skycastle/group/model/vo/Groups.java
+++ src/main/java/com/kh/skycastle/group/model/vo/Groups.java
@@ -1,5 +1,19 @@
 package com.kh.skycastle.group.model.vo;
 
+import java.sql.Date;
+
 public class Groups {
+	
+	private int groupNo;
+	private int leaderNo;
+	private String groupTitle;
+	private String groupSubtitle;
+	private String groupCategory;
+	private String place;
+	private int gradeLimit;
+	private int memberLimit;
+	private String status;
+	private Date startDate;
+	private Date endDate;
 
 }
