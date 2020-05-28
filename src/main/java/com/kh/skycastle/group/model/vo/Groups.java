fe486f019d1fc4b4b530f8b08235e9c50943df64
YEONHWAJO <63631237+YEONHWAJO@users.noreply.github.com>
2020-05-28 20:42:21 +0900

    groups vo 완성

--- src/main/java/com/kh/skycastle/group/model/vo/Groups.java
+++ src/main/java/com/kh/skycastle/group/model/vo/Groups.java
@@ -2,6 +2,18 @@ package com.kh.skycastle.group.model.vo;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Groups {
 	
 	private int groupNo;
