6c0edfdfa2ca196c8c64736046a5d6737ec0c8d8
YEONHWAJO <63631237+YEONHWAJO@users.noreply.github.com>
2020-05-29 01:29:57 +0900

    Create Reply.java

---
+++ src/main/java/com/kh/skycastle/group/model/vo/Reply.java
@@ -0,0 +1,14 @@
+package com.kh.skycastle.group.model.vo;
+
+import java.sql.Date;
+
+public class Reply {
+	
+	private int replyNo;
+	private String replyContent;
+	private int noticeNo;
+	private int userNo;
+	private Date uploadDate;
+	private String status;
+
+}
