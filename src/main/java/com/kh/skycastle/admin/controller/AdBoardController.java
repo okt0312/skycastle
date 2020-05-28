960c68ff38068148727efdd6d0badf2a3248feb8
yunny406 <59042188+yunny406@users.noreply.github.com>
2020-05-29 04:53:41 +0900

    게시글 관리 수정
    주소 뒤에 jsp수정

--- src/main/java/com/kh/skycastle/admin/controller/AdBoardController.java
+++ src/main/java/com/kh/skycastle/admin/controller/AdBoardController.java
@@ -10,13 +10,13 @@ public class AdBoardController {
 		//메뉴바에서 공지사항관리 페이지 눌렀을때
 		@RequestMapping("noticeMgmt.ad")
 		public String adNoticeMgmt(){
-			return "admin/adNoticeMgmt.jsp";
+			return "admin/adNoticeMgmt";
 		}
 		
 		//메뉴바에서 이벤트관리 페이지 눌렀을때
 		@RequestMapping("eventMgmt.ad")
 		public String adEventMgmt(){
-			return "admin/adEventMgmt.jsp";
+			return "admin/adEventMgmt";
 		}
 				
 		
