c755b4aa5a6b6abb429bea7c2cdb2b0e6d3f67da
YEONHWAJO <63631237+YEONHWAJO@users.noreply.github.com>
2020-05-29 21:11:54 +0900

    Update GroupNoticeController.java

--- src/main/java/com/kh/skycastle/group/controller/GroupNoticeController.java
+++ src/main/java/com/kh/skycastle/group/controller/GroupNoticeController.java
@@ -1,5 +1,12 @@
 package com.kh.skycastle.group.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class GroupNoticeController {
+	
+	@RequestMapping("groupApplyForm.gr")
+	public String mygroupList() {
+		return "group/groupApplyForm";
+	}
 
 }
