9bcbe541148788ab74c459fb41560f2d39929056
shinyoungKim <keemsy112@gmail.com>
2020-05-29 21:43:29 +0900

    Qna vo  추가

--- src/main/java/com/kh/skycastle/cs/model/vo/Qna.java
+++ src/main/java/com/kh/skycastle/cs/model/vo/Qna.java
@@ -1,5 +1,27 @@
 package com.kh.skycastle.cs.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Qna {
 
+	private int qnaNo;
+	private String memberYn;	// 회원여부 
+	private String userName;
+	private String email;
+	private int userNo;
+	private String qnaTitle;
+	private String qnaContent;
+	private String qnaReply;
+	private String originName;
+	private String changeName;
+	private String status;
 }
