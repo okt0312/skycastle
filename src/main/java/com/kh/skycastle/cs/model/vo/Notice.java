a2319540b443fa40dc69164e4334ebb72efcb132
shinyoungKim <keemsy112@gmail.com>
2020-05-29 21:37:48 +0900

    Notice vo 추가

--- src/main/java/com/kh/skycastle/cs/model/vo/Notice.java
+++ src/main/java/com/kh/skycastle/cs/model/vo/Notice.java
@@ -1,5 +1,23 @@
 package com.kh.skycastle.cs.model.vo;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Notice {
 
+	private int noticeNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private Date enrollDate;
+	private String status;
 }
