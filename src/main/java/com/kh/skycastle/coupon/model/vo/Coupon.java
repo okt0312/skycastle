470a0d0f4173d145b5a8894f9dd8beabf3a9bf7d
xorud3290 <xorud3290@naver.com>
2020-06-01 16:59:34 +0900

    쿠폰 vo

--- src/main/java/com/kh/skycastle/coupon/model/vo/Coupon.java
+++ src/main/java/com/kh/skycastle/coupon/model/vo/Coupon.java
@@ -1,5 +1,26 @@
 package com.kh.skycastle.coupon.model.vo;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class Coupon {
+	
+	private String couponCode;
+	private String couponName;
+	private Date startDate;
+	private int expirationPeriod;
+	private int discountRate;
 
 }
