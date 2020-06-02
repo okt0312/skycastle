06a3c8590f5a6e903c01f4d06d947fb903b914e7
yunny406 <59042188+yunny406@users.noreply.github.com>
2020-06-02 17:48:21 +0900

    소모임조회 디에오 ,소모임 컨트롤러 수정
    소모임조회 수정관련 디에오 컨트롤러 수정

--- src/main/java/com/kh/skycastle/admin/model/dao/AdGroupsDao.java
+++ src/main/java/com/kh/skycastle/admin/model/dao/AdGroupsDao.java
@@ -6,18 +6,28 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.skycastle.groups.model.vo.Groups;
+import com.kh.skycastle.member.model.vo.Member;
 
 //소모임 관리(소모임조회, 개설관리, 소모임게시글관리)관련 Dao입니다
 @Repository
 public class AdGroupsDao {
 	
-	//게시글 리스트 불러오는 메소드
+	//소모임조회 리스트 불러오는 메소드
 	public ArrayList<Groups> selectGroups(SqlSessionTemplate sqlSession){
 		return (ArrayList)sqlSession.selectList("adGroupsMapper.selectGroups");
 	}
 	
+	//소모임 조회 모달에서 수정하는 메소드
+	public int updateGrMgmt(SqlSessionTemplate sqlSession, Groups g)
+	{
+		return sqlSession.update("adGroupsMapper.updateGrMgmt", g);
+	}
 	
-	//게시글 리스트 불러오는 메소드
+	
+	
+	
+	
+	//소모임개설관리 리스트 불러오는 메소드
 		public ArrayList<Groups> selectGrConfirm(SqlSessionTemplate sqlSession){
 			return (ArrayList)sqlSession.selectList("adGroupsMapper.selectGrConfirm");
 		}
