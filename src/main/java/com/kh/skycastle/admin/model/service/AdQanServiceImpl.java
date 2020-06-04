24a5f93511409bac52abafd7eed07fa11116b90d
okt0312 <62687161+okt0312@users.noreply.github.com>
2020-06-04 16:06:12 +0900

    qna mybatis-config 및 mvc 설정

---
+++ src/main/java/com/kh/skycastle/admin/model/service/AdQanServiceImpl.java
@@ -0,0 +1,26 @@
+package com.kh.skycastle.admin.model.service;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.skycastle.admin.model.dao.AdQnaDao;
+import com.kh.skycastle.cs.model.vo.Qna;
+
+@Service("adqService")
+public class AdQanServiceImpl implements AdQnaService{
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private AdQnaDao adqDao;
+	
+	@Override
+	public ArrayList<Qna> selectQnaList() {
+		return adqDao.selectQnaList(sqlSession);
+	}
+
+}
